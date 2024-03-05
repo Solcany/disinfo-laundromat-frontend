fa5780ecd14c830ef2be510a27c1a50e38958cdc
m <matus.solcany@protonmail.com>
2024-03-05 17:22:16 +0100

    InputCheckboxGroup comp

---
+++ src/lib/components/InputCheckboxGroup.svelte
@@ -0,0 +1,28 @@
+<script>
+  // WIP: implement this with bits ui checkbox
+  // make this ts compliant
+  //import Checkbox from 'bits-ui';
+  const items = ["a", "b", "c"]
+  let selected = [];
+
+  function toggleAll(e) {
+    selected = e.target.checked ? [...items] : [];
+  }
+</script>
+
+<div>
+    {selected.length} Selected
+    <label>
+        <input 
+            type="checkbox" 
+            on:change={toggleAll} 
+            checked={selected.length === items.length} />
+        <strong>All</strong>
+    </label>
+    {#each items as item}
+        <label>
+            <input type="checkbox" bind:group={selected} name={item} value={item}  />
+            {item}
+        </label>
+    {/each}
+</div>
