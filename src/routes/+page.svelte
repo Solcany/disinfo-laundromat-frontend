87e6fc69bbf477b763fdc1debf7f32760297f551
m <matus.solcany@protonmail.com>
2024-04-02 11:27:16 +0200

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,40 +23,6 @@
   import { contentStore, metadataStore } from '$stores/apiData.ts';
   export let data;
 
-  async function handleSubmit(event: Event, query: { type: QueryType; endpoint: Endpoint }) {
-    event.preventDefault();
-    loadingStore.set(true);
-    const target = event.target as HTMLFormElement;
-    let formData = new FormData(target);
-
-    if (query.endpoint === Endpoint.ParseUrl || query.endpoint === Endpoint.Content) {
-      formData.set('combineOperator', 'OR');
-    } else if (query.endpoint === Endpoint.Fingerprint && !formData.has('run_urlscan')) {
-      formData.set('run_urlscan', '0');
-    }
-    const response: ApiResponse<ApiContentData | ApiFingerprintData> = await queryApi(
-      query.type,
-      query.endpoint,
-      formData
-    );
-    if (response.error) {
-    } else {
-      if (response.data) {
-        if (query.endpoint === Endpoint.Content) {
-          contentFormDataStore.set(formData);
-          contentStore.set(response.data as ApiContentData);
-          goto('/search/content');
-        } else if (query.endpoint === Endpoint.Fingerprint) {
-          metadataStore.set(response.data as ApiFingerprintData);
-          goto('/search/metadata');
-        }
-        loadingStore.set(false);
-      } else {
-        loadingStore.set(false);
-      }
-    }
-  }
-
   $: contentBasicFormConfig = data.contentBasicFormConfig;
   $: metadataBasicFormConfig = data.metadataBasicFormConfig;
 </script>
@@ -99,19 +65,18 @@
           {#if contentBasicFormConfig}
             <Form
               config={contentBasicFormConfig}
-              onSubmit={handleSubmit}
+              onSubmit={handleApiSubmit}
               orientation={FormOrientation.Horizontal}
             />
           {/if}
         </C.Content>
         <C.Content value="technical similarity">
           <P>
-            Enter a website URL to analyse metadata similarity with a variety of media outlets,
-            including both trusted, dubious and known disinformation news sources.
-            <br /><br />
 
-            This tool is particularly helpful to detect affiliate networks, sister sites,
-            advertising collaborations, or DNS ban circumvention practices.
+          Enter a website URL to analyse metadata similarity with a variety of media outlets, including both trusted, dubious and known disinformation news sources.
+          <br><br/>
+
+          This tool is particularly helpful to detect affiliate networks, sister sites, advertising collaborations, or DNS ban circumvention practices.
           </P>
           {#if metadataBasicFormConfig}
             <Form
@@ -129,41 +94,25 @@
       <li class="">
         <H2 class="pb-4">Prove content laundering from state-owned outlets</H2>
         <P>
-          Researchers from two European Universities, supported by the Alliance for Securing
-          Democracy, reveal evidence of a small network operating in Poland and Germany that
-          launders Russian state media content into mainstream media. A sample of articles from
-          problematic outlets, criticizing Ukrainian President Zelensky, was run through the
-          Laundromat’s content search. The tool revealed that all the articles in the sample were
-          lifted from Ria Novosti, an outlet belonging to the Russian? Segodnya group. Read the
-          repor
-        </P>
+          Researchers from two European Universities, supported by the Alliance for Securing Democracy, reveal evidence of a small network operating in Poland and Germany that launders Russian state media content into mainstream media. A sample of articles from problematic outlets, criticizing Ukrainian President Zelensky, was run through the Laundromat’s content search. The tool revealed that all the articles in the sample were lifted from Ria Novosti, an outlet belonging to the Russian? Segodnya group. Read the repor
+          </P>
       </li>
       <li class="pt-6 md:pt-0">
         <H2 class="pb-4">Find proxy websites that exist to overcome DNS bans</H2>
         <P>
-          Both Russia Today and Sputnik News are currently subjected to DNS bans imposed by the
-          European Commission. However, despite these restrictions, each site employs sophisticated
-          techniques to reach audiences in EU member countries. Read the report to learn how
-          researchers were able to utilize the Laundromat's website fingerprinting service to map
-          the entire network of Russia Today proxy websites, using altered or entirely new domain
-          names.
+        Both Russia Today and Sputnik News are currently subjected to DNS bans imposed by the European Commission. However, despite these restrictions, each site employs sophisticated techniques to reach audiences in EU member countries. Read the report to learn how researchers were able to utilize the Laundromat's website fingerprinting service to map the entire network of Russia Today proxy websites, using altered or entirely new domain names.
         </P>
       </li>
       <li class="pt-6 md:pt-0">
         <H2 class="pb-4">Tell if a site produces original content</H2>
         <P>
-          Content laundering occurs when individuals or entities replicate website content without
-          proper attribution and, in some cases, authorization. This method is commonly employed by
-          disinformation operatives to disseminate problematic content and political propaganda. The
-          Laundromat tool serves as an effective means to detect duplicate content online and
-          substantiate instances of content plagiarism. See how to use the Laundromat to prove gaps
-          in editorial transparency. Read the guide.
+        Content laundering occurs when individuals or entities replicate website content without proper attribution and, in some cases, authorization. This method is commonly employed by disinformation operatives to disseminate problematic content and political propaganda. The Laundromat tool serves as an effective means to detect duplicate content online and substantiate instances of content plagiarism. See how to use the Laundromat to prove gaps in editorial transparency. Read the guide.
         </P>
       </li>
     </ul>
   </div>
 </section>
-<section class="flex flex-col md:flex-row">
+<section class="w-full flex flex-col md:flex-row md:no-wrap">
   <div>
     <H3></H3>
     <ul>
@@ -172,9 +121,7 @@
           <img
             src="https://camo.githubusercontent.com/4644af047750ba3f45a2f3efcf9f746857215547882628e6e2a34ca806cc72ac/68747470733a2f2f7365637572696e6764656d6f63726163792e676d6675732e6f72672f77702d636f6e74656e742f75706c6f6164732f323032342f30322f454d49465f486f72697a6f6e74616c5f6c6f676f5f426c61636b2e706e67"
             alt="MJRC Logo"
-            height="256"
             data-canonical-src="https://securingdemocracy.gmfus.org/wp-content/uploads/2024/02/EMIF_Horizontal_logo_Black.png"
-            style="width: 100%;height:auto"
           />
         </a>
       </li>
@@ -183,34 +130,31 @@
 
   <div>
     <H3></H3>
-    <ul>
-      <li>
+    <ul class="flex items-center">
+      <li class="h-10">
         <a href="https://securingdemocracy.gmfus.org/" rel="nofollow">
           <img
             src="https://securingdemocracy.gmfus.org/wp-content/uploads/2023/05/ASD-Logo.png"
             alt="ASD Logo"
             data-canonical-src="https://securingdemocracy.gmfus.org/wp-content/uploads/2020/02/ASD_Logo_Black.png"
-            style="width: 100%;height:auto"
           />
         </a>
       </li>
       <li>
         <a href="https://www.isdglobal.org/" rel="nofollow">
-          <img
-            src="https://securingdemocracy.gmfus.org/wp-content/uploads/2024/03/ISD-logo-ISD-Red.png"
-            alt="ISD Logo"
-            data-canonical-src="https://securingdemocracy.gmfus.org/wp-content/uploads/2020/02/ISD_Logo_Black.png"
-            style="width: 100%;height:auto"
-          />
+            <img
+              src="https://securingdemocracy.gmfus.org/wp-content/uploads/2024/03/ISD-logo-ISD-Red.png"
+              alt="ISD Logo"
+              data-canonical-src="https://securingdemocracy.gmfus.org/wp-content/uploads/2020/02/ISD_Logo_Black.png"
+              class=" w-full object-contain"/>
         </a>
       </li>
-      <li>
+      <li class="h-10">
         <a href="https://www.uva.nl/en" rel="nofollow">
           <img
             src="https://securingdemocracy.gmfus.org/wp-content/uploads/2024/03/uva-logo-uvamerken_eng-1406x236-300dpi.png"
             alt="UvA Logo"
             data-canonical-src="https://securingdemocracy.gmfus.org/wp-content/uploads/2020/02/UvA_Logo_Black.png"
-            style="width: 100%;height:auto"
           />
         </a>
       </li>
