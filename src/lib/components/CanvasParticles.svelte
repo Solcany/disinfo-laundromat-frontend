c2849216e7b1ba0d12d870149acb79d468d7c38c
m <matus.solcany@protonmail.com>
2024-04-25 16:32:21 +0200

    ensure engine loads only once

--- src/lib/components/CanvasParticles.svelte
+++ src/lib/components/CanvasParticles.svelte
@@ -3,16 +3,20 @@
     import { loadSlim } from '@tsparticles/slim';
     import { cn } from '$utils';
     import { CANVAS_PARTICLES_CONFIG } from '$config';
+    import { isParticlesEngineLoadedStore } from '$stores/particlesEngine.ts'
 
     let className: string | undefined = undefined;
     export { className as class} 
-
+    
     void particlesInit(async (engine: any) => {
-        await loadSlim(engine);
+        if (!$isParticlesEngineLoadedStore ) {
+            await loadSlim(engine);
+            console.log("engine loaded!");
+            isParticlesEngineLoadedStore.set(true);
+        }
     });
 </script>
 
 <Particles
         class={cn("", className)}
-        options={CANVAS_PARTICLES_CONFIG}
-/>
+        options={CANVAS_PARTICLES_CONFIG}/>
