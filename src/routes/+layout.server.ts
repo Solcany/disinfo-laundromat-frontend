1c9350d7f31692af72cb24655291fa690e95f625
m <matus.solcany@protonmail.com>
2024-05-17 15:19:29 +0200

    remove unused logs

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,7 +2,6 @@ import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ cookies }) => {
 	const gdpr_consent = cookies.get('gdpr_consent');
-  console.log("got cookie server load", gdpr_consent);
 
 	return {
 		gdpr_consent: gdpr_consent,
