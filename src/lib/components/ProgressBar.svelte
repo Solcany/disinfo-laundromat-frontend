78f965ad93fadaa629b883f4f25fce6ffd13a30c
m <matus.solcany@protonmail.com>
2024-05-28 12:55:52 +0200

    fix overflow

--- src/lib/components/ProgressBar.svelte
+++ src/lib/components/ProgressBar.svelte
@@ -6,21 +6,41 @@
 
   let progressValue = 100; // 0;
   let isLoading = false;
-  //let timer: number;
 
   let className: string = '';
   export { className as class };
+
+  let value = 0;
+  let set = true;
+  onMount(() => {
+    //timer = 100;
+    const interval = setInterval(()=> {
+      console.log("tick");
+      if(set) {
+        value = 0;
+        set = !set;
+      } else {
+        value = 100;
+        set = !set;
+      }
+    }, 1500)
+    //const timer = setTimeout(() => (value = 100), 1000);
+    //return () => clearTimeout(timer);
+  });
+
 </script>
 
-{#if $loadingStore}
+{#if true}
   <Progress.Root
-    value={progressValue}
+    value={value}
     max={100}
-    class={cn('shadow-mini-inset relative h-[2px] w-screen overflow-hidden', className)}
+    class={cn('absolute left-0 top-0 w-full h-[5px] overflow-hidden', className)}
   >
     <div
-      class="shadow-mini-inset absolute h-full w-full flex-1 bg-blue-500 transition-all duration-1000 ease-linear"
-      style={`transform: translateX(-${100 - (100 * (progressValue ?? 0)) / (100 ?? 1)}%)`}
+      class="absolute h-full w-screen flex-1 bg-blue-500 transition-all duration-1000 ease-linear"
+      style={`transform: translateX(-${
+          100 - (100 * (value ?? 0)) / (100 ?? 1)
+          }%)`}
     />
   </Progress.Root>
 {/if}
