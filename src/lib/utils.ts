436330b1483a43519ea167a924f381fbb6ff08ff
m <matus.solcany@protonmail.com>
2024-06-11 16:22:01 +0200

    remove unused utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,39 +6,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function includeObjectKeys(
-  obj: Record<string, any>,
-  allowed: string[]
-): Record<string, any> {
-  const filtered = Object.keys(obj)
-    .filter((key) => allowed.includes(key))
-    .reduce(
-      (newObj, key) => {
-        key = key as keyof typeof newObj;
-        newObj[key] = obj[key];
-        return newObj;
-      },
-      {} as Record<string, any>
-    );
-  return filtered;
-}
-
-export function excludeObjectKeys(
-  obj: Record<string, any>,
-  allowed: string[]
-): Record<string, any> {
-  const filtered = Object.keys(obj)
-    .filter((key) => !allowed.includes(key))
-    .reduce(
-      (newObj, key) => {
-        key = key as keyof typeof newObj;
-        newObj[key] = obj[key];
-        return newObj;
-      },
-      {} as Record<string, any>
-    );
-  return filtered;
-}
 
 export function getPathRoot(path: string): string {
   const segments = path.split('/');
@@ -46,19 +13,6 @@ export function getPathRoot(path: string): string {
   return '/' + root;
 }
 
-export function isNumber(value: any): value is number {
-  return typeof value === 'number' && !isNaN(value);
-}
-
-export function zip(...arrays: any[][]): any[][] {
-  const length = arrays.length > 0 ? arrays[0].length : 0;
-  for (const arr of arrays) {
-    if (arr.length !== length) {
-      throw new Error('Arrays must have the same length');
-    }
-  }
-  return Array.from({ length }, (_, index) => arrays.map((array) => array[index]));
-}
 
 export function domainToUrl(domain: string) {
   if (!domain.startsWith('http://') && !domain.startsWith('https://')) {
